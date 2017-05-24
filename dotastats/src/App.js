5afb93031994e250ade6d74ad15d9cb073ffd0a3
fearzen3 <ivanhabi2@gmail.com>
2017-05-24 10:46:26 +0700

    done

--- dotastats/src/App.js
+++ dotastats/src/App.js
@@ -11,13 +11,14 @@ class App extends Component {
     heroes : []
   }
  }
-  
+
   componentWillMount(){
     let self = this
   axios.get('https://api.opendota.com/api/heroStats')
   .then(function (response) {
+    console.log(response.data);
     self.setState({heroes : response.data})
-   
+
   })
   .catch(function (error) {
     console.log(error);
